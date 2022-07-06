27c904708549e73ffd9ac36e46947186c4825d78
Chris Kratz <Ckratz17@gmail.com>
2022-07-06 09:17:18 -0400

    more functions

--- Assets/script.js
+++ Assets/script.js
@@ -72,3 +72,58 @@ function setUvColor(uvi) {
     }else return purple
 }
 
+function searchWeather(queryURl) {
+
+    $.ajax({
+        url: queryURl,
+        method: 'Get'
+    }).then(function (response){
+
+        let city = response.name;
+        let id = response.id;
+        if(pastCities[0]) {
+            pastCities = $.grep(pastCities, function (storedCity) {
+                return id !== storedCity.id;
+            })
+        }
+        pastCities.unshift({ city, id });
+        storeCities();
+        displayCities(pastCities);
+
+        cityEl.text(response.name);
+        let formattedDate = moment.unix(response.dt).format('L');
+        dateEl.text(formattedDate);
+        let weatherIcon = response.weather[0].icon;
+        weatherIconEl.attr('src', `http://openweathermap.org/img/wn/${weatherIcon}.png`).attr('alt', response.weather[0].description)
+        tempEl.html(((response.main.temp - 273.15) * 1.8 + 32).toFixed(1))
+        humidEl.text(response.main.humidity)
+        windEl.text((response.wind.speed * 2.237).toFixed(1))
+
+        let lat = response.coord.lat;
+        let lon = response.coord.lon;
+        let urlAll = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+        $ajax({
+            url: urlAll,
+            method: 'Get'
+        }).then(function (response) {
+            let uvIndex = response.current.uvi;
+            let uvColor = setUvColor(uvIndex)
+            uvEl.text(response.current.uvi)
+            uvEl.attr('style', 'background-color: ${uvColor}; color: ${uvColor === "yellow" ? "black" : "white"}')
+            let fiveDay = response.daily;
+
+            for (let i = 0; i <= 5; i++) {
+                let currDay = fiveDay[i];
+                $(`div.day-${i} .card-title`).text(moment.unix(currDay.dt).format('L'))
+                $(`div.day-${i} .fiveDay-img`).attr(
+                    'src',
+                    `http://openweathermap.org/img/wn/${currDay.weather[0].icon}.png`
+                ).attr('alt', currDay.weather[0].description);
+                $(`div.day-${i} .fiveDay-temp`).text(((currDay.temp.day - 273.15) * 1.8 + 32).toFixed(1));
+                $(`div.day-${i} .fiveDay-humid`).text(currDay.humidity)
+                $(`div.day-${i} .fiveDay-wind`).text((response.wind.speed * 2.237).toFixed(1))
+            }
+        })
+    })
+}
