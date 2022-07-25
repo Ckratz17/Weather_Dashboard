914118697248815d384c11b084f8f9ceeced6d24
Chris Kratz <Ckratz17@gmail.com>
2022-07-24 21:02:11 -0400

    api link and skeleton for the functions set up

---
+++ Assets/script/script.js
@@ -0,0 +1,11 @@
+var apiKey = "d1e2d0763204896fd894698f5c6e27ee";
+var today = moment().format('L')
+var searchHistory = []
+
+function currentWeather {
+//function to display the current weather from the api and append it to the page
+}
+
+function futureForecast {
+//function to display the 5 day forecast from the api and append it to the page
+}
